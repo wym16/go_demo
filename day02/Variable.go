0664d3fccb450f1a2539b2020fc5e7a7ed2954ec
wym <228364863@qq.com>
2023-08-04 00:03:07 +0800

    feat:variable

---
+++ day02/Variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var str = "day02"
+	fmt.Println(str)
+
+	var intA, intB int = 1, 2
+
+	fmt.Println(intA)
+	fmt.Println(intB)
+	fmt.Println(intA + intB)
+
+	var intC int
+	fmt.Println(intC)
+
+	var boolA bool
+	fmt.Println(boolA)
+
+	var boolB bool = true
+	fmt.Println(boolB)
+
+	var test = false
+	fmt.Println(test)
+}
