c475a4ca83e4e305a3f720639f16b71d523f9737
wym <228364863@qq.com>
2023-08-16 23:07:41 +0800

    feat:Compute

---
+++ day04/Compute.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 12
+	var b int = 10
+	var c int
+
+	c = a + b
+	fmt.Printf("第一行 - c 的值为 %d\n", c)
+	c = a - b
+	fmt.Printf("第二行 - c 的值为 %d\n", c)
+	c = a * b
+	fmt.Printf("第三行 - c 的值为 %d\n", c)
+	c = a / b
+	fmt.Printf("第四行 - c 的值为 %d\n", c)
+	c = a % b
+	fmt.Printf("第五行 - c 的值为 %d\n", c)
+	a++
+	fmt.Printf("第六行 - a 的值为 %d\n", a)
+	a = 12 // a重新赋值为
+	a--
+	fmt.Printf("第七行 - a 的值为 %d\n", a)
+}
